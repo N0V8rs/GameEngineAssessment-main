e690a92287c69ed66c230ed2583141eff2492b84
N0V8rs <144738466+N0V8rs@users.noreply.github.com>
2024-03-01 15:22:04 -0400

    UI Score Added
    Score text, score -15 and +15 buttons added.

--- Assets/Scripts/ScoreCounter.cs
+++ Assets/Scripts/ScoreCounter.cs
@@ -1,31 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using TMPro;
-using JetBrains.Annotations;
-using TMPro.EditorUtilities;
-using UnityEngine.UIElements;
-using UnityEngine.UI;
+using UnityEngine;
 
 public class ScoreCounter : MonoBehaviour
 {
-    public int score = 0;
-    public TMP_Text Score;
+    private int score = 0;
+    public  TMP_Text scoreText;
 
     public void Awake()
     {
-        
-        Score.text = score.ToString();
+        scoreText.text = "Score: " + score.ToString();
     }
-    public void ButtonUP(int value)
+
+    public void ButtonUP(int value) // Value == in Unity to asigned the value in the inspector
     {
-        if (value == 0)
+        if (value == 0) // value = 0
         {
             score = 0;
         }
-        
-        score += value;
 
-        Score.text = score.ToString();
+        score += value; // Adds the score
+
+        scoreText.text = "Score: " + score.ToString(); // Displays the text
     }
 }
